d06b2d851a624d7faa376370a5af9fae95c1e974
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-12-12 03:01:29 +0100

    day 2

--- day2_2.js
+++ day2_2.js
@@ -18,12 +18,18 @@ readInterface
     data.forEach((element) => {
       let strings = element.split(" ");
       let values = strings[0].split("-");
-      let min = values[0];
-      let max = values[1];
+      let first = values[0] - 1;
+      let second = values[1] - 1;
       let letter = strings[1][0];
       let pass = strings[2];
-      let occurrences = pass.split(letter).length - 1;
-      if (min <= occurrences && occurrences <= max) {
+      let matches = 0;
+      if (pass[first] == letter) {
+        matches++;
+      }
+      if (pass[second] == letter) {
+        matches++;
+      }
+      if (matches == 1) {
         count++;
       }
     });
